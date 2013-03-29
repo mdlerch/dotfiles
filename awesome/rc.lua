9618a25cd82e4f7db36f6adca87776bd8c1a8875
Michael Lerch <mdlerch@gmail.com>
2013-03-29 10:08:39 -0600

    launch urxtc with tmux

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -56,7 +56,7 @@ end
 beautiful.init("/home/mike/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "urxvtc"
+terminal = "/home/mike/bin/myterm"
 editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
