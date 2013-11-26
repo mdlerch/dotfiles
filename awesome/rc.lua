299cee3d7f0681368a9bdf762cc08f8322c454b2
Michael Lerch <mdlerch@gmail.com>
2013-11-25 18:48:39 -0700

    no more conky

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -574,7 +574,7 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 --
 -- os.execute("nm-applet --sm-disable &")
 -- os.execute("wicd &")
-os.execute("/home/mike/bin/myconky")
+-- os.execute("/home/mike/bin/myconky")
 
 
 -- vim: fdm=marker
