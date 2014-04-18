835a784c6a1af2de5d5c74e5dc7cf0ddd20836e0
Michael Lerch <mdlerch@gmail.com>
2014-04-18 10:57:28 -0600

    use higher quality image

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -73,7 +73,7 @@ theme.titlebar_maximized_button_normal_active = "/usr/share/awesome/themes/defau
 theme.titlebar_maximized_button_focus_active  = "/usr/share/awesome/themes/default/titlebar/maximized_focus_active.png"
 
 -- theme.wallpaper = "/data/wallpapers/mypic2/lakeplateau.jpg"
-theme.wallpaper = "/home/mike/in/windows_xp_bliss-1280x800.jpg"
+theme.wallpaper = "/home/mike/in/windows_xp_bliss-1920x1200.jpg"
 
 -- You can use your own layout icons like this:
 theme.layout_fairh = "/usr/share/awesome/themes/default/layouts/fairhw.png"
