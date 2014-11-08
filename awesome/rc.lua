48dc62f05562b4a68ea9f1c30240ad2fd473b916
Michael Lerch <mdlerch@gmail.com>
2014-11-08 13:58:18 -0700

    Random wallpaper every 5 minutes

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -132,45 +132,61 @@ mytextclock = awful.widget.textclock(" %a %b %d, %I:%M ", 20)
 -- {{{ Theming
 
 beautiful.init("/home/mike/.config/awesome/theme.lua")
-if beautiful.wallpaper then
-    for s = 1, screen.count() do
-        gears.wallpaper.maximized(beautiful.wallpaper, s, true)
-    end
-end
+-- if beautiful.wallpaper then
+--     for s = 1, screen.count() do
+--         gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+--     end
+-- end
 
 -- }}}
--- {{{ Random Wallpapers (NOT IMPLEMENTED)
+-- {{{ Random Wallpapers
 
--- Get the list of files from a directory. Must be all images or folders and non-empty. 
--- function scanDir(directory)
--- local i, fileList, popen = 0, {}, io.popen
--- for filename in popen([[find "]] ..directory.. [[" -type f):lines() do
--- i = i + 1
--- fileList[i] = filename
--- end
--- return fileList
--- end
--- wallpaperList = scanDir("/data/wallpapers/mypics/")
+-- function to return list of wallpapers
+function scandir(directory, filter)
+    local i, t, popen = 0, {}, io.popen
+    if not filter then
+        filter = function(s) return true end
+    end
+    print(filter)
+    for filename in popen('ls -a "'..directory..'"'):lines() do
+        if filter(filename) then
+            i = i + 1
+            t[i] = filename
+        end
+    end
+    return t
+end
 
--- Apply a random wallpaper on startup.
--- gears.wallpaper.maximized(wallpaperList[math.random(1, #wallpaperList)], s, true)
+wp_index = 1
+wp_timeout = 300
+wp_path = "/data/wallpapers/"
+wp_filter = function(s) return string.match(s, "%.png$") or string.match(s, "%.jpg$") end
+wp_files = scandir(wp_path, wp_filter)
 
--- Apply a random wallpaper every changeTime seconds.
--- changeTime = 60
--- wallpaperTimer = timer { timeout = changeTime }
--- wallpaperTimer:connect_signal("timeout", function()
--- gears.wallpaper.maximized(wallpaperList[math.random(1, #wallpaperList)], s, true)
+wp_index = math.random(1, #wp_files)
+for s = 1, screen.count() do
+    gears.wallpaper.maximized(wp_path .. wp_files[wp_index], s, true)
+end
 
--- stop the timer (we don't need multiple instances running at the same time)
--- wallpaperTimer:stop()
 
--- restart the timer
--- wallpaperTimer.timeout = changeTime
--- wallpaperTimer:start()
--- end)
+wp_timer = timer { timeout = wp_timeout }
+wp_timer:connect_signal("timeout", function()
+    for s = 1, screen.count() do
+        gears.wallpaper.maximized(wp_path .. wp_files[wp_index], s, true)
+    end
 
--- initial start when rc.lua is first run
--- wallpaperTimer:start()
+    wp_timer:stop()
+
+    wp_index = math.random(1, #wp_files)
+
+    wp_timer.timeout = wp_timeout
+
+    wp_timer.timeout = wp_timeout
+    wp_timer:start()
+end)
+
+wp_timer:start()
+
 
 -- }}}
 -- {{{ Aliases
