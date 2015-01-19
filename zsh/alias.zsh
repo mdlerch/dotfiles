cefa934177ec3d726628c82b45fa3df459c4d726
Michael Lerch <mdlerch@gmail.com>
2015-01-18 21:45:49 -0700

    Updates

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -6,12 +6,12 @@ alias ls='ls --color=auto --group-directories-first'
 alias mv='mv -i'
 alias grep='grep --color'
 alias n='urxvtc &'
-alias rsyncwork='rsync -av --size-only /data/work /var/run/media/mike/storage/rsync.d'
-alias rsyncwallpapers='rsync -av --size-only /data/wallpapers /var/run/media/mike/storage/rsync.d'
-alias rsyncpictures='rsync -av --size-only /data/pictures /var/run/media/mike/storage/rsync.d'
-alias rsyncpodcasts='rsync -av --size-only /data/podcasts /var/run/media/mike/storage/rsync.d'
-alias rsyncmusic='rsync -av --size-only /data/music /var/run/media/mike/storage/rsync.d'
-alias rsyncdotfiles='rsync -av --size-only /home/mike/dotfiles /var/run/media/mike/storage/rsync.d'
+alias rsyncwork='rsync -av --size-only /data/work /media/storage/rsync.d'
+alias rsyncwallpapers='rsync -av --size-only /data/wallpapers /media/storage/rsync.d'
+alias rsyncpictures='rsync -av --size-only /data/pictures /media/storage/rsync.d'
+alias rsyncpodcasts='rsync -av --size-only /data/podcasts /media/storage/rsync.d'
+alias rsyncmusic='rsync -av --size-only /data/music /media/storage/rsync.d'
+alias rsyncdotfiles='rsync -av --size-only /home/mike/dotfiles /media/storage/rsync.d'
 alias R='R --no-save --no-restore-data'
 alias gauss='ssh gauss.math.montana.edu -l lerch'
 alias gaussx='ssh -X gauss.math.montana.edu -l lerch'
@@ -27,7 +27,7 @@ alias knoxcareer='cadaver https://knox.montana.edu/CareerServices'
 alias combinepdf='gs -dNOPAUSE -sDEVICE=pdfwrite -sOUTPUTFILE=combinedpdf.pdf -dBATCH '
 alias unix2dos='dos2unix -D'
 alias skype='xhost +local: && sudo -u skype /usr/bin/skype'
-alias mocp="if [ "$TMUX" ]; then tmux setw monitor-activity off; tmux move-window -t 9; tmux rename-window 'mocp'; fi; mocp -A"
 alias mutt="if [ "$TMUX" ]; then tmux move-window -t 8; tmux rename-window 'mutt'; fi; mutt"
 alias cmus="player"
+alias pynchclock="if [ "$TMUX" ]; then tmux setw monitor-activity off; tmux move-window -t 9; tmux rename-window 'time'; fi; cd ~/work/time/; pynchclock"
 #alias ipython="python /usr/bin/ipython"
